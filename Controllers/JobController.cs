b8f70ef56326fdeaf8de60401b0196bb1357667c
Ritesh <riteshrairajput11@gmail.com>
2023-10-08 15:02:15 +0500

    Added Job controller and a view

---
+++ Controllers/JobController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineJobPortal.Controllers
+{
+    public class JobController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
