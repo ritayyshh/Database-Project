d303ce682e9af2da083841bd331d59d7c7af52db
Emmanuel <emmenual123@gmail.com>
2023-10-08 18:50:13 +0500

    Added null check

--- ClientApp/src/components/hello.js
+++ ClientApp/src/components/hello.js
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
 
 const Hello = () => {
-  const [data, setData] = useState('')
+  const [data, setData] = useState(null)
   useEffect(() => {
     fetch('job/12345')
       .then((results) => {
         return results.json()
       })
       .then(data => {
-        setData(data)
+          setData(data)
       })
   }, [])
 
-  return (
-      <>
-          <h1>{data[0].job_title}</h1>
-      </>
-  )
+    if (data != null) {
+        return (
+            <>
+                <h1>{data[0].job_title}</h1>
+            </>
+        )
+    }
+  
 }
 export default Hello;
