7acda019edaa993dd951e6ea1db1ec9821b2e599
Emmanuel <emmenual123@gmail.com>
2023-10-08 15:52:08 +0500

    Made changes

--- ClientApp/src/components/hello.js
+++ ClientApp/src/components/hello.js
@@ -3,11 +3,11 @@ import { useEffect, useState } from "react";
 const Hello = () => {
   const [data, setData] = useState('')
   useEffect(() => {
-    const fetchPromise = fetch('job');
+    const fetchPromise = fetch('Job/Index');
     fetchPromise.then(response => {
       setData(response.data)
     })
-  })
+  }, [])
 
   return (
     {data}
