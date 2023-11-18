e5ffa471402ff9283601441775918f2db0169190
Ritesh <riteshrairajput11@gmail.com>
2023-11-18 17:07:37 +0500

    Added dummy data in job controller

--- Controllers/JobController.cs
+++ Controllers/JobController.cs
@@ -4,17 +4,45 @@ using OnlineJobPortal.Models;
 namespace OnlineJobPortal.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[jobcontroller]")]
     public class JobController : ControllerBase
     {
         private static readonly IEnumerable<JobsModel> Jobs = new[]
         {
-            new JobsModel
+           new JobsModel
             {
-                job_id=12345, company_id="9876543210", job_title="Software Engineer",
+                job_id=12345, company_id=1, job_title="Software Engineer",
                 job_description="This is a dummy job description for a Software Engineer position.",
                 job_type="Full-time", job_location="San Francisco, CA", job_salary=100000,
                 job_post_date="2023-10-08", job_deadline="2023-10-22"
+            },
+            new JobsModel
+            {
+                job_id=12346, company_id=2, job_title="Web Developer",
+                job_description="This is a dummy job description for a Web Developer position.",
+                job_type="Contract", job_location="New York, NY", job_salary=90000,
+                job_post_date="2023-10-10", job_deadline="2023-10-24"
+            },
+            new JobsModel
+            {
+                job_id=12347, company_id=3, job_title="Data Scientist",
+                job_description="This is a dummy job description for a Data Scientist position.",
+                job_type="Part-time", job_location="Seattle, WA", job_salary=120000,
+                job_post_date="2023-10-12", job_deadline="2023-10-26"
+            },
+            new JobsModel
+            {
+                job_id=12348, company_id=4, job_title="UX/UI Designer",
+                job_description="This is a dummy job description for a UX/UI Designer position.",
+                job_type="Full-time", job_location="Austin, TX", job_salary=95000,
+                job_post_date="2023-10-15", job_deadline="2023-10-29"
+            },
+            new JobsModel
+            {
+                job_id=12349, company_id=5, job_title="Network Administrator",
+                job_description="This is a dummy job description for a Network Administrator position.",
+                job_type="Contract", job_location="Chicago, IL", job_salary=85000,
+                job_post_date="2023-10-18", job_deadline="2023-11-01"
             }
         };
         [HttpGet("{job_id:int}")]
--- Models/JobsModel.cs
+++ Models/JobsModel.cs
@@ -3,13 +3,13 @@
     public class JobsModel
     {
         public int job_id { get; set; }
-        public string? company_id { get; set; }
-        public string? job_title { get; set; }
-        public string? job_description { get; set;}
-        public string? job_type { get; set;}
-        public string? job_location { get; set;}
+        public int company_id { get; set; }
+        public string job_title { get; set; }
+        public string job_description { get; set;}
+        public string job_type { get; set;}
+        public string job_location { get; set;}
         public int job_salary { get; set; }
-        public string? job_post_date { get; set; }
-        public string? job_deadline { get; set;}
+        public string job_post_date { get; set; }
+        public string job_deadline { get; set;}
     }
 }
